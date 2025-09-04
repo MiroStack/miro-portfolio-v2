2a5a17d4dca7446de238b59ec914db5daca1d78f
Miro Ivan D. Sarte <mirosarte.work@gmail.com>
2025-09-04 17:06:42 +0800

    My first commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-gray-900 text-white flex flex-col justify-center items-center">
+         <h1>Hi I'm Miro Ivan D. Sarte</h1>
+    </div>
+  );
+}
+
+export default App;
